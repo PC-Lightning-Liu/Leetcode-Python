e7fcd6421f5bc029c02c1d9f54154278553ff13f
PC-Lightning-Liu <pcliu9703@163.com>
2017-08-15 11:02:49 +0800

    Update Twosum.py

--- Twosum.py
+++ Twosum.py
@@ -4,3 +4,16 @@ class Solution(object):
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+                
+# 更快的处理方法
+
+class Solution:
+    def twoSum(self, num, target):
+        map = {}
+        for i in range(len(num)):
+            if num[i] not in map:
+                map[target - num[i]] = i + 1
+            else:
+                return map[num[i]], i + 1
+        return -1, -1
+# 查找是否在字典的key里面，复杂度是O（n）
